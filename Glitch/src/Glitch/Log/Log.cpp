5d60fba991ea8685e5c8c681ddfb378541182aeb
Wouter van Hees <whjhees@avans.nl>
2020-10-13 11:31:08 +0200

    added beginnen to log function to expand functionality of logger

--- Glitch/src/Glitch/Log/Log.cpp
+++ Glitch/src/Glitch/Log/Log.cpp
@@ -13,4 +13,8 @@ namespace Glitch {
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
+	void Log::LogInfo(...)
+	{
+		
+	}
 }
--- Glitch/src/Glitch/Log/Log.h
+++ Glitch/src/Glitch/Log/Log.h
@@ -3,6 +3,7 @@
 #include "spdlog/spdlog.h"
 #include <spdlog/fmt/ostr.h>
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <stdarg.h>   
 
 namespace Glitch {
 	class GLITCH_API Log
@@ -13,19 +14,19 @@ namespace Glitch {
 		static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
 		static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 
+		void LogInfo(...);
+
 	private:
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 	};
 }
 
-#define GL_CORE_FATAL(...)	::Glitch::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 #define GL_CORE_ERROR(...)	::Glitch::Log::GetCoreLogger()->error(__VA_ARGS__)
 #define GL_CORE_WARN(...)	::Glitch::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define GL_CORE_INFO(...)	::Glitch::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define GL_CORE_TRACE(...)	::Glitch::Log::GetCoreLogger()->trace(__VA_ARGS__)
 
-#define GL_FATAL(...)	::Glitch::Log::GetClientLogger()->fatal(__VA_ARGS__)
 #define GL_ERROR(...)	::Glitch::Log::GetClientLogger()->error(__VA_ARGS__)
 #define GL_WARN(...)	::Glitch::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define GL_INFO(...)	::Glitch::Log::GetClientLogger()->info(__VA_ARGS__)
