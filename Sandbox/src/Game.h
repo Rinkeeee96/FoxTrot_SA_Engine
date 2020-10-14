ee547bdbc710dae3085df1a7830754feb9576954
WilliamRossAvans <44771477+WilliamRossAvans@users.noreply.github.com>
2020-10-14 17:18:41 +0200

    Add standing char+ list all+ game settings

--- Sandbox/src/Game.h
+++ Sandbox/src/Game.h
@@ -5,14 +5,16 @@ class Game : public Glitch::Application
 public:
 	Game() {
 		sceneManager.createNewScene(1);
+;
+		renderer->loadSingleSprite(1, "../Assets/Sprites/World/LIGHT TILE WITHOUT TOP.png");
 
-		renderer->loadSingleSprite(1, "../Assets/Sprites/Project assets/Adventurer-1.5/Individual Sprites/adventurer-air-attack1-00.png");
-		renderer->loadSingleSprite(101, "../Assets/Sprites/World/LIGHT TILE WITHOUT TOP.png");
-		renderer->loadSprite(2, "../Assets/Sprites/Character/adventure_air_attack1.png", 37, 250, 4);
-		renderer->loadSprite(3, "../Assets/Sprites/Character/adventure_run.png", 37, 250, 6);
-		renderer->loadSprite(4, "../Assets/Sprites/Character/adventure_slide.png", 37, 250, 2);
 
-		Glitch::Object* object = new Glitch::Object(1);
+		renderer->loadSingleSprite(100, "../Assets/Sprites/Character/adventure.png");
+		renderer->loadSprite(101, "../Assets/Sprites/Character/adventure_air_attack1.png", 37, 50, 4);
+		renderer->loadSprite(102, "../Assets/Sprites/Character/adventure_run.png", 37, 50, 6);
+		renderer->loadSprite(103, "../Assets/Sprites/Character/adventure_slide.png", 37, 50, 2);
+
+		Glitch::Object* object = new Glitch::Object(100);
 		object->setName("character");
 		object->setHeight(200);
 		object->setWidth(200);
@@ -24,12 +26,13 @@ public:
 		object->setFriction(0);
 		object->setRestitution(0);
 		object->setStatic(false);
-		object->registerSprite("air_attack", 2);
-		object->registerSprite("run", 3);
-		object->registerSprite("slide", 4);
+		object->registerSprite("air_attack", 101);
+		object->registerSprite("run", 102);
+		object->registerSprite("slide", 103);
+		object->changeTextureToState("run");
 		sceneManager.getSceneWithID(1)->addNewObject(object);
 
-		Glitch::Object* staticGround = new Glitch::Object(101);
+		Glitch::Object* staticGround = new Glitch::Object(1);
 		staticGround->setWidth(500); // width
 		staticGround->setHeight(10);// height
 		staticGround->setPositionX(20); // x 20 left down
