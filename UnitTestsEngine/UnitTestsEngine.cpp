9eb3eb5449748616fdbdadb154fc919061af60a4
Lars Jansen <ljphjans@avans.nl>
2020-11-02 20:12:22 +0100

    Added unit tests projects not working WIP

---
+++ UnitTestsEngine/UnitTestsEngine.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../engineFoxtrot/api.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestsEngine
+{
+	TEST_CLASS(UnitTestsEngine)
+	{
+	public:
+		Engine engine;
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
---
+++ UnitTestsGlitch/UnitTestsGlitch.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestsGlitch
+{
+	TEST_CLASS(UnitTestsGlitch)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
