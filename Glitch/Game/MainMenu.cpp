792a24065e1a4776c99beced1dd405efe1ff9732
Wouter van Hees <whjhees@avans.nl>
2020-11-09 15:55:18 +0100

    added temporary center of screen values

--- Glitch/Game/MainMenu.cpp
+++ Glitch/Game/MainMenu.cpp
@@ -2,13 +2,18 @@
 #include "Events/Mouse/MousePressed.h"
 
 #define BIND_FN(function) std::bind(&MainMenu::function, *this)
+#define CENTER_X  1920 / 2
+#define CENTER_Y 1080 / 2
+
+
 
 void MainMenu::OnAttach()
 {
 	Button startBtn(1, "start", Color(0, 0, 0), BIND_FN(OnStartBtnClick));
 	startBtn.setSize(100, 50);
-	startBtn.setPositionX(500);
-	startBtn.setPositionY(500);
+
+	startBtn.setPositionX(CENTER_X);
+	startBtn.setPositionY(CENTER_Y);
 
 	addNewObjectToLayer(1, &startBtn);
 }
