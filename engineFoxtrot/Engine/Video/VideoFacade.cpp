b7ad50b35207cb93db1ae7f7b9815c37583c2d5f
Rinke de Vries <Rinkedevries@gmail.com>
2020-11-09 13:54:38 +0100

    Added static rectangle for tracking the player

--- engineFoxtrot/Engine/Video/VideoFacade.cpp
+++ engineFoxtrot/Engine/Video/VideoFacade.cpp
@@ -74,6 +74,13 @@ void VideoFacade::clearScreen()
 void VideoFacade::drawScreen()
 {
 	try {
+		SDL_Rect r;
+		r.x = 785;
+		r.y = 365;
+		r.w = 350;
+		r.h = 350;
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderDrawRect(renderer, &r);
 		SDL_RenderPresent(renderer);
 	}
 	catch (...) {
