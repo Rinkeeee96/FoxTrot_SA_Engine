05737c259d2751310a31e8777d0339be6dd147be
Lars Jansen <ljphjans@avans.nl>
2020-11-17 20:14:43 +0100

    Fixed merge issue

--- engineFoxtrot/Engine/SceneManager/Scene.h
+++ engineFoxtrot/Engine/SceneManager/Scene.h
@@ -45,11 +45,6 @@ public:
 	/// If any work needs to be done during a scene this is were to place it
 	virtual void update() = 0;
 
-	/// @brief
-	/// run is called in the main loop on the currentScene
-	/// If any work needs to be done during a scene this is were to place it
-	virtual void update() = 0;
-
 	void setSceneWidth(const int width) { sceneWidth = width; }
 	int getSceneWidth() const { return sceneWidth; }
 
