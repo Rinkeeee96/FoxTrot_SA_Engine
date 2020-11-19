e89c6994684da3987837b157c6b356f63b5f5825
Thijs <ttleeuw96@gmail.com>
2020-11-19 10:42:56 +0100

    Added down event for ai

--- Glitch/Game/Characters/Enemies/Slime.h
+++ Glitch/Game/Characters/Enemies/Slime.h
@@ -42,7 +42,15 @@ public:
 	}
 
 	void onUpdate() override {
-		if(this->getYAxisVelocity() == 0) EventSingleton::get_instance().dispatchEvent<ActionEvent>((Event&)ActionEvent(Direction::UP, this->getObjectId()));
+		if(this->getYAxisVelocity() == 0) EventSingleton::get_instance().dispatchEvent<ActionEvent>((Event&)ActionEvent(Direction::UP, this->getObjectId()));\
+
+		// TODO Fire down event when player under slime
+		else if (player->getPositionX() >= this->getPositionX() && player->getPositionX() + player->getWidth() <= this->getPositionX()) {
+			if (player->getPositionY() <= this->getPositionY() && player->getPositionY() + player->getHeight() >= this->getPositionY()) {
+				//player->getPositionY() < this->getPositionY()
+				EventSingleton::get_instance().dispatchEvent<ActionEvent>((Event&)ActionEvent(Direction::DOWN, this->getObjectId()));
+			}
+		}
 	};
 
 	ICharacter* clone(int id) override { return new Slime(id); }
