a2f8997096a28118e20eb4a9fe7a57d3b108d00b
WilliamRossAvans <44771477+WilliamRossAvans@users.noreply.github.com>
2020-11-21 11:07:46 +0100

    Revert "Fix input"
    This reverts commit adb00daf998e40be2d675f23fec16724c1d11991.

--- Glitch/Game/Levels/Level.cpp
+++ Glitch/Game/Levels/Level.cpp
@@ -64,7 +64,6 @@ void Level::start() {
 void Level::onUpdate() {
 	if (this->win) {
 		SceneSwitcher::get_instance().switchToScene("WIN_SCREEN", false);
-		player->kill();
 		return;
 	}
 	if (player->getIsDead()) {
