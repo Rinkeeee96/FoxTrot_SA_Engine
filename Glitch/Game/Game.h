4d6e8800f4fed0fb255f423fd552fe9fe924d314
Rinke de Vries <Rinke@scentronix.com>
2020-11-23 13:57:39 +0100

    Fix includes

--- Glitch/Game/Game.h
+++ Glitch/Game/Game.h
@@ -2,11 +2,6 @@
 #include "Game/SpriteState.h"
 #include "Game/Characters/Player/Player.h"
 #include "SceneSwitcher/SceneSwitcher.h"
-#include "Game/Scenes/MainMenu/MainMenu.h"
-#include "Game/Scenes/Transitions/GeneralTransition/GeneralTransition.h"
-#include "Game\Scenes\Overworld\Overworld.h"
-#include "Game/Scenes/DeathScreen/DeadScreen.h"
-#include "Game/Scenes/WinScreen/WinScreen.h"
 #include "Game/Scenes/Scenes.h"
 
 /// @brief 
@@ -20,6 +15,8 @@ public:
 
 private:
 	Engine engine;
+
+	unique_ptr<Scene> currentScene;
 	bool gameRunning = true;
 	int sceneId = 0;
 };
