29eea85294b1ebd6cf924605ecba0a0a7b130b01
Lars Jansen <ljphjans@avans.nl>
2020-11-27 09:55:49 +0100

    Added API to functions in particle adapter

--- engineFoxtrot/Engine/ParticleSystem/ParticleAdapter.h
+++ engineFoxtrot/Engine/ParticleSystem/ParticleAdapter.h
@@ -11,17 +11,17 @@ public:
 	API ~ParticleAdapter() {};
 
 	API void onUpdate() override {};
-	void API setPositionX(const float pos) { this->positionX = pos; setXPosition((int)pos); };
-	float API getPositionX() const { return this->positionX; };
+	API void setPositionX(const float pos) { this->positionX = pos; setXPosition((int)pos); };
+	API float getPositionX() const { return this->positionX; };
 
-	void API setPositionY(const float pos) { this->positionY = pos; setYPosition((int)pos); };
-	float API getPositionY() const { return this->positionY; };
+	API void setPositionY(const float pos) { this->positionY = pos; setYPosition((int)pos); };
+	API float getPositionY() const { return this->positionY; };
 
-	void setWidth(float width) { this->width = width; }
-	float getWidth() const { return this->getWidth(); }
+	API void setWidth(float width) { this->width = width; }
+	API float getWidth() const { return this->getWidth(); }
 
-	void setHeight(float Height) { this->height = Height; }
-	float getHeight() const { return this->getHeight(); }
+	API void setHeight(float Height) { this->height = Height; }
+	API float getHeight() const { return this->getHeight(); }
 
 private:
 
