7f16c5c5762a8b228b69b57dea1b8616c9f6ca11
Wouter van Hees <whjhees@avans.nl>
2020-11-28 15:02:38 +0100

    created first version of keypress invoker

--- Glitch/Commands/KeypressInvoker.h
+++ Glitch/Commands/KeypressInvoker.h
@@ -6,13 +6,65 @@ class KeypressInvoker
 public:
 	KeypressInvoker();
 	~KeypressInvoker();
+	/// @brief
+	/// Register keybinds in the invoker for the first time, 
+	/// throws exception when trying to register multiple commands under the same binding
+	/// @param code
+	/// The keycode for the command in the map
+	/// @param command
+	/// A shared pointer to the command which needs to be registered
+	void registerCommands(KeyCode code, shared_ptr<ICommand> command) {
+		if (isRegistered(command)) throw exception("keycode already registered, cannot override");
 
-	void registerCommands(KeyCode code, ICommand command);
-	void executeCommand(KeyCode code);
+		playerCommands[code] = command;
+	};
+	/// @brief
+	/// Update the keycode that is set to this command, unbinds the previously registerd command if present
+	/// @param code
+	/// The new keycode for the command in the map
+	/// @param command
+	/// A shared pointer to the command which needs to be updated
+	void updateCommand(KeyCode code, shared_ptr<ICommand> command) {
+		// unbind existing command if it has a key bound to it, if so, unbind it
+		if (isRegistered(command))
+		{
+			unboundCommands.push_back(command);
+			playerCommands.erase(code);
+		}
+		// set the command
+		playerCommands[code] = command;
+	};
+	/// @brief
+	/// Attempts to execute a command for a given keycode,
+	/// fails silently when no bind is registered to prevent unwanted exceptions
+	/// @param code
+	/// Keycode for the to execute command
+	void executeCommand(KeyCode code) {
+		if (isRegistered(code)) {
+			playerCommands[code]->execute();
+		}
+	};
 
 private:
-	unordered_map<KeyCode, ICommand*> playerCommands;
-	ICharacter* player;
+	// TODO transfer ownership to command invoker from builder via unique_ptr, I don't intend to keep the builder alive once the initial keybinds have been loaded
+	unordered_map<KeyCode, shared_ptr<ICommand>> playerCommands;
+	vector<shared_ptr<ICommand>> unboundCommands;
+
+	bool isRegistered(shared_ptr<ICommand> command) {
+		auto it = playerCommands.begin();
+		while (it != playerCommands.end())
+		{
+			if (compare_shared_ptr<ICommand, ICommand>(it->second, command))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool isRegistered(const KeyCode& keyCode) {
+		return playerCommands.find(keyCode) != playerCommands.end();
+	}
 };
 
 KeypressInvoker::KeypressInvoker()
--- Glitch/pch.h
+++ Glitch/pch.h
@@ -19,11 +19,18 @@
 #include <ctime>
 #include "api.h"
 
-
 #include "SceneSwitcher/SceneSwitcher.h"
 #include "SceneManager/Objects/Button.h"
 #include "SceneSwitcher/SceneSwitcher.h"
 #include "Game/SaveGame/Savegame.h"
 
+// also returns false when comparing null value to shared ptr
+template<class T, class U>
+bool compare_shared_ptr(const std::shared_ptr<T>& a, const std::shared_ptr<U>& b)
+{
+	if (a == b) return true;
+	if (a && b) return *a == *b;
+	return false;
+}
 
 using namespace std;
