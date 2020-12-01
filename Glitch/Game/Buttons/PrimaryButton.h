5e8db479bd51a370ba0c68dcc722d542f7265c69
Rinke de Vries <Rinkedevries@gmail.com>
2020-12-01 20:49:24 +0100

    Revert "Some more consts"
    This reverts commit 8a337e0cf9790df273e918873b6f729ead2ecd59.

--- Glitch/Game/Buttons/PrimaryButton.h
+++ Glitch/Game/Buttons/PrimaryButton.h
@@ -5,7 +5,7 @@
 class PrimaryButton : public Button
 {
 public:
-	PrimaryButton(int id, string _text, const function<void(void)> _onClick,const EventDispatcher& _dispatcher) : 
+	PrimaryButton(int id, string _text, const function<void(void)> _onClick, EventDispatcher& _dispatcher) : 
 		Button(id, ColoredText(_text, Color(0,0,0)), _onClick, new SpriteObject(101010, 40, 116, 1, 300, "Assets/Buttons/btn_green_round.png"), _dispatcher) {
 		auto hoverPrimaryBtn = new SpriteObject(101020, 40, 116, 1, 300, "Assets/Buttons/btn_brown_round.png");
 		hasHoverSprite = true;
