27a9a76c4a13df6919f5040bdcde40fe46adbd2f
Thijs <ttleeuw96@gmail.com>
2020-12-02 14:00:52 +0100

    done?

--- Glitch/Game/Scenes/Credits/CreditsScene.cpp
+++ Glitch/Game/Scenes/Credits/CreditsScene.cpp
@@ -13,10 +13,34 @@
 void CreditsScene::onAttach()
 {
 	loadButtons();
+	loadText();
 	loadBackground();
 	loadMusic();
 }
 
+/// @brief 
+/// Create all text for this scene
+void CreditsScene::loadText() {
+	auto* name1 = new Text(4, new ColoredText("Lars Jansen", Color(0, 0, 0)), SHORT_NAME_WIDTH, TEXT_HEIGHT, CENTER_X - LEFT_X_CENTER, CENTER_Y - 150);
+	auto* name2 = new Text(5, new ColoredText("Max van Nistelrooij", Color(0, 0, 0)), LONG_NAME_WIDTH, TEXT_HEIGHT, CENTER_X - LEFT_X_CENTER, CENTER_Y + 50);
+	auto* name3 = new Text(6, new ColoredText("Rinke de Vries", Color(0, 0, 0)), SHORT_NAME_WIDTH, TEXT_HEIGHT, CENTER_X - LEFT_X_CENTER, CENTER_Y + 250);
+	auto* name4 = new Text(7, new ColoredText("Thijs de leeuw", Color(0, 0, 0)), SHORT_NAME_WIDTH, TEXT_HEIGHT, CENTER_X + RIGHT_X_CENTER, CENTER_Y - 150);
+	auto* name5 = new Text(8, new ColoredText("William Ross", Color(0, 0, 0)), SHORT_NAME_WIDTH, TEXT_HEIGHT, CENTER_X + RIGHT_X_CENTER, CENTER_Y + 50);
+	auto* name6 = new Text(9, new ColoredText("Wouter van Hees", Color(0, 0, 0)), LONG_NAME_WIDTH, TEXT_HEIGHT, CENTER_X + RIGHT_X_CENTER, CENTER_Y + 250);
+	auto* name7 = new Text(10, new ColoredText("Foxtrot", Color(0, 0, 0)), 500, 75, CENTER_X - 350, CENTER_Y + 650);
+	this->text.push_back(name1);
+	this->text.push_back(name2);
+	this->text.push_back(name3);
+	this->text.push_back(name4);
+	this->text.push_back(name5);
+	this->text.push_back(name6);
+	this->text.push_back(name7);
+
+	for (size_t i = 0; i < this->text.size(); i++)
+	{
+		addNewObjectToLayer(3, this->text[i]);
+	}
+}
 /// @brief 
 /// Create all buttons for this scene
 void CreditsScene::loadButtons() {
@@ -28,59 +52,60 @@ void CreditsScene::loadButtons() {
 	backBtn->setPositionX(WINDOW_WIDTH - 40 - backBtn->getWidth());
 	backBtn->setPositionY(WINDOW_HEIGHT - 10 - backBtn->getHeight());
 
-	auto* name1 = new Text(4, new ColoredText("Lars Jansen", Color(255, 255, 255)), 300, 50, CENTER_X - 500, CENTER_Y - 150);
-	auto* name2 = new Text(5, new ColoredText("Max van Nistelrooij", Color(255, 255, 255)), 350, 50, CENTER_X - 500, CENTER_Y + 50);
-	auto* name3 = new Text(6, new ColoredText("Rinke de Vries", Color(255, 255, 255)), 300, 50, CENTER_X - 500, CENTER_Y + 250);
-
-	auto* name4 = new Text(7, new ColoredText("Thijs de leeuw", Color(255, 255, 255)), 300, 50, CENTER_X + 50, CENTER_Y - 150);
-	auto* name5 = new Text(8, new ColoredText("William Ross", Color(255, 255, 255)), 300, 50, CENTER_X + 50, CENTER_Y + 50);
-	auto* name6 = new Text(9, new ColoredText("Wouter van Hees", Color(255, 255, 255)), 350, 50, CENTER_X + 50, CENTER_Y + 250);
-
-	//addNewObjectToLayer(3, level2Btn);
 	addNewObjectToLayer(3, backBtn);
-	addNewObjectToLayer(3, name1);
-	addNewObjectToLayer(3, name2);
-	addNewObjectToLayer(3, name3);
-	addNewObjectToLayer(3, name4);
-	addNewObjectToLayer(3, name5);
-	addNewObjectToLayer(3, name6);
-
-	this->text.push_back(name1);
-	this->text.push_back(name2);
-	this->text.push_back(name3);
-	this->text.push_back(name4);
-	this->text.push_back(name5);
-	this->text.push_back(name6);
 }
 
 /// @brief 
 /// Create the background for this scene
 void CreditsScene::loadBackground() {
-	SpriteObject* BG_LAYER_0 = new SpriteObject(-1010, 1080, 1920, 1, 300, "Assets/Backgrounds/menu_Layer_0.png");
+	SpriteObject* bg = new SpriteObject(-1012, 1080, 1920, 1, 300, "Assets/Backgrounds/credits_foreground.png");
+	auto* bgLayer = new Drawable(13);
+	bgLayer->setStatic(true);
+	bgLayer->setPositionX(0);
+	bgLayer->setPositionY(1080);
+	bgLayer->setWidth(1920);
+	bgLayer->setHeight(1080);
+	bgLayer->registerSprite(SpriteState::DEFAULT, bg);
+	bgLayer->changeToState(SpriteState::DEFAULT);
 
-	auto* layer0 = new Drawable(14);
-	layer0->setStatic(true);
-	layer0->setPositionX(1);
-	layer0->setPositionY(1080);
-	layer0->setWidth(1920);
-	layer0->setHeight(1080);
-	layer0->registerSprite(SpriteState::DEFAULT, BG_LAYER_0);
-	layer0->changeToState(SpriteState::DEFAULT);
+	SpriteObject* fgTop = new SpriteObject(-1010, 1080, 1920, 1, 300, "Assets/Backgrounds/credits_foreground_top.png");
 
-	addNewObjectToLayer(0, layer0, false, true);
+	auto* foregroundTop = new Drawable(14);
+	foregroundTop->setStatic(true);
+	foregroundTop->setPositionX(0);
+	foregroundTop->setPositionY(0 + 220);
+	foregroundTop->setWidth(1920);
+	foregroundTop->setHeight(220);
+	foregroundTop->registerSprite(SpriteState::DEFAULT, fgTop);
+	foregroundTop->changeToState(SpriteState::DEFAULT);
+
+	SpriteObject* BG_LAYER_1 = new SpriteObject(-1011, 1080, 1920, 1, 300, "Assets/Backgrounds/credits_foreground_bottom.png");
+
+	auto* foregroundBottom = new Drawable(15);
+	foregroundBottom->setStatic(true);
+	foregroundBottom->setPositionX(0);
+	foregroundBottom->setPositionY(1080);
+	foregroundBottom->setWidth(1920);
+	foregroundBottom->setHeight(250);
+	foregroundBottom->registerSprite(SpriteState::DEFAULT, BG_LAYER_1);
+	foregroundBottom->changeToState(SpriteState::DEFAULT);
+
+	addNewObjectToLayer(0, bgLayer, false, true);
+	addNewObjectToLayer(4, foregroundTop, false, true);
+	addNewObjectToLayer(4, foregroundBottom, false, true);
 }
 
 /// @brief 
 /// Load the sounds for this scene
 void CreditsScene::loadMusic() {
-	engine.soundEngine.onLoadBackgroundMusicEvent("DEAD_SOUND", "Assets/Sound/game_over_looped.wav");
+	engine.soundEngine.onLoadBackgroundMusicEvent("WIN_SOUND", "Assets/Sound/TremLoadingloopl.wav");
 }
 
 /// @brief 
 /// Create the sounds for this scene
 void CreditsScene::start()
 {
-	engine.soundEngine.onStartBackgroundMusicEvent("DEAD_SOUND");
+	engine.soundEngine.onStartBackgroundMusicEvent("WIN_SOUND");
 }
 
 void CreditsScene::onUpdate()
@@ -88,10 +113,7 @@ void CreditsScene::onUpdate()
 	for (size_t i = 0; i < this->text.size(); i++)
 	{
 		this->text[i]->setPositionY(this->text[i]->getPositionY() - 1);
-
-		if (text[i]->getPositionY() == 0) {
-			this->text[i]->setPositionY(WINDOW_HEIGHT);
-		}
+		if (text[i]->getPositionY() == 0) this->text[i]->setPositionY(WINDOW_HEIGHT);
 	}
 }
 
@@ -99,7 +121,7 @@ void CreditsScene::onUpdate()
 /// Remove the sounds of the soundengine
 void CreditsScene::onDetach()
 {
-	engine.soundEngine.onStartBackgroundMusicEvent("DEAD_SOUND");
+	engine.soundEngine.onStartBackgroundMusicEvent("WIN_SOUND");
 	Scene::onDetach();
 }
 
--- Glitch/Game/Scenes/Credits/CreditsScene.h
+++ Glitch/Game/Scenes/Credits/CreditsScene.h
@@ -3,6 +3,12 @@
 #include "SceneManager/Objects/Button.h"
 #include "Game/Scenes/GameScene.h"
 
+#define SHORT_NAME_WIDTH 300
+#define LONG_NAME_WIDTH 350
+#define LEFT_X_CENTER 500
+#define RIGHT_X_CENTER 50
+#define TEXT_HEIGHT 50
+
 class CreditsScene : public GameScene
 {
 public:
@@ -17,6 +23,7 @@ private:
 
 	vector<Text*> text;
 
+	void loadText();
 	void loadBackground();
 	void loadMusic();
 	void loadButtons();
