c2f2cc1e25564343c3852f4f529270acf915b280
Rinke de Vries <Rinkedevries@gmail.com>
2020-12-09 14:36:56 +0100

    Basic implementation inventory

--- Glitch/Game/PopUps/Inventory/InventoryPopup.cpp
+++ Glitch/Game/PopUps/Inventory/InventoryPopup.cpp
@@ -1,2 +1,51 @@
 #include "pch.h"
 #include "InventoryPopup.h"
+#include "Game/Buttons/PrimaryButton.h"
+#include "Game/SpriteState.h"
+
+#define BIND_FN(function) std::bind(&InventoryPopup::function, *this)
+
+InventoryPopup::InventoryPopup(EventDispatcher& _dispatcher, SceneStateMachine& _stateMachine) :
+	dispatcher(_dispatcher), stateMachine(_stateMachine)
+{
+}
+
+InventoryPopup::~InventoryPopup()
+{
+}
+
+/// @brief Setup the Pause Pop Up.
+void InventoryPopup::setupPopUp()
+{
+	Drawable* background = new Drawable(-564574);
+	SpriteObject* backgroundSprite = new SpriteObject(-564577, 200, 500, 1, 1, "Assets/Sprites/PopUp/PopUpBlackBorder.png");
+	Text* text = new Text(-564573, new ColoredText("Paused", Color(0, 0, 0), false),
+		200, 50, WINDOW_WIDTH_CENTER - 100, WINDOW_HEIGHT_CENTER - 175);
+
+	PrimaryButton* backButton = new PrimaryButton(-564572, "Close", BIND_FN(onBackButtonClick), dispatcher);
+	backButton->setHeight(50);
+	backButton->setWidth(200);
+	backButton->setPositionX(WINDOW_WIDTH_CENTER - 100);
+	backButton->setPositionY(WINDOW_HEIGHT_CENTER - 100);
+	backButton->setDrawStatic(true);
+
+	background->setHeight(300);
+	background->setWidth(500);
+	background->setDrawStatic(true);
+
+	background->setPositionX(WINDOW_WIDTH_CENTER - 250);
+	background->setPositionY(WINDOW_HEIGHT_CENTER);
+
+	background->registerSprite(SpriteState::DEFAULT, backgroundSprite);
+	background->changeToState(SpriteState::DEFAULT);
+
+	text->setDrawStatic(true);
+	addObjectInLayer(backButton);
+	addObjectInLayer(text);
+	addObjectInLayer(background);
+}
+
+void InventoryPopup::onBackButtonClick()
+{
+	stateMachine.switchToScene("Overworld", false);
+}
--- Glitch/Game/PopUps/Inventory/InventoryPopup.h
+++ Glitch/Game/PopUps/Inventory/InventoryPopup.h
@@ -1,5 +1,21 @@
 #pragma once
-class InventoryPopup
+#include "Game/Scenes/Statemachine/SceneStateMachine.h"
+
+
+
+/// @brief Pause Pop Up
+class InventoryPopup : public Layer
 {
+public:
+	InventoryPopup(EventDispatcher& _dispacther, SceneStateMachine& _stateMachine);
+	~InventoryPopup();
+
+	void setupPopUp();
+
+private:
+	EventDispatcher& dispatcher;
+	SceneStateMachine& stateMachine;
+
+	void onBackButtonClick();
 };
 
