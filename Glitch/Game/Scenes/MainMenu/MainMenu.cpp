484ed5915c68c2443d7953abbf2e5ff56c805d2b
Lars Jansen <ljphjans@avans.nl>
2020-12-09 14:47:33 +0100

    Fixed white border

--- Glitch/Game/Scenes/MainMenu/MainMenu.cpp
+++ Glitch/Game/Scenes/MainMenu/MainMenu.cpp
@@ -49,7 +49,7 @@ void MainMenu::loadBackground() {
 
 	auto* layer0 = new Drawable(14);
 	layer0->setStatic(true);
-	layer0->setPositionX(1);
+	layer0->setPositionX(0);
 	layer0->setPositionY(1080);
 	layer0->setWidth(1920);
 	layer0->setHeight(1080);
@@ -68,7 +68,7 @@ void MainMenu::loadBackground() {
 	
 	auto* layer2 = new Drawable(16);
 	layer2->setStatic(true);
-	layer2->setPositionX(1);
+	layer2->setPositionX(0);
 	layer2->setPositionY(1080);
 	layer2->setWidth(1920);
 	layer2->setHeight(1080);
