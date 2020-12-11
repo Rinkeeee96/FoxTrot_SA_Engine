3beb9d43abcd0a1eef863123a705f8c32132daa7
Wouter van Hees <whjhees@avans.nl>
2020-12-11 13:42:20 +0100

    Update CommandBuilder.h

--- Glitch/Game/Commands/Builder/CommandBuilder.h
+++ Glitch/Game/Commands/Builder/CommandBuilder.h
@@ -16,13 +16,11 @@ public:
 
 	// Inherited via ICommandBuilder
 	void buildPlayerCommands(Player& player, GameKeypressInvoker* invoker) override;
-	void linkCommandToToggle(GameKeypressInvoker* invoker, int layerId, UICommand* command);
-
+	void linkCommandToToggle(GameKeypressInvoker* invoker, int layerId, const string& identifier) override;
 private:
 	shared_ptr<CharacterCommandFactory> characterCommandFactory;
 	shared_ptr<UICommandFactory> uiCommandFactory;
 
-	// Inherited via ICommandBuilder
 	void initCharacterFactory();
 	void initGlobalFactory();
 };
