964edeb5b5fdd4338f4bbd47765fa804689818fe
Wouter van Hees <whjhees@avans.nl>
2020-12-11 14:45:15 +0100

    removed bullshit from toggle fps command

--- engineFoxtrot/Engine/Input/Commands/Engine/ToggleFpsCommand.h
+++ engineFoxtrot/Engine/Input/Commands/Engine/ToggleFpsCommand.h
@@ -1,25 +1,18 @@
 #include "stdafx.h"
 #include "Input/Commands/ICommand.h"
 #include "video/VideoEngine.h"
+
 class EventDispatcher;
 class ToggleFpsCommand : public ICommand
 {
 public:
 	ToggleFpsCommand(VideoEngine& _engine): engine{ _engine }, ICommand("toggleFps") {}
 
-	void dispatchPauseEvent(EventDispatcher& dispatcher, bool state) {
-		TogglePauseEvent pauseEvent(state);
-		dispatcher.dispatchEvent<TogglePauseEvent>(pauseEvent);
-
-		lastPauseState = state;
-	}
-
 	// Inherited via ICommand
 	void execute(EventDispatcher& dispatcher) override
 	{
 		engine.toggleFps();
 	}
-
 private:
 	VideoEngine& engine;
 	bool lastPauseState = false;
