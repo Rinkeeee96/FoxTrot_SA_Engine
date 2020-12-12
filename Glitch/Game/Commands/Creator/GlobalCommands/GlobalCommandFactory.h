7f17d2477a91c2aedec987a43a0efb3807d8e2db
Wouter van Hees <whjhees@avans.nl>
2020-12-12 16:30:56 +0100

    removed doxygen from commandfactory header

--- Glitch/Game/Commands/Creator/GlobalCommands/GlobalCommandFactory.h
+++ Glitch/Game/Commands/Creator/GlobalCommands/GlobalCommandFactory.h
@@ -6,13 +6,7 @@ class IGlobalCommandCreator;
 class GlobalCommandFactory
 {
 public:
-	/// @brief
-	/// Function for registrating a command creator
-	/// a command is created by this creator upon request with its registered classname
 	void registerit(const std::string& classname, IGlobalCommandCreator* creator);
-	/// @brief
-	/// Function for creating ui commands and registering a layer id
-	/// a command is created by this creator upon request with its registered classname
 	GlobalCommand* createCommand(const std::string& classname, const int layerIndex);
 private:
 	map<string, IGlobalCommandCreator*> table;
