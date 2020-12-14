3954d8065f426a6dd703de788944d1b8c68bd82a
Thijs <ttleeuw96@gmail.com>
2020-12-14 17:45:44 +0100

    video facade tests

--- engineFoxtrot.tests/FoxtrotTests/src/Video/VideoFacadeTests.cpp
+++ engineFoxtrot.tests/FoxtrotTests/src/Video/VideoFacadeTests.cpp
@@ -12,14 +12,74 @@ namespace UnitTestsEngine
 	TEST_CLASS(VideoFacadeTests)
 	{
 	public:
-		TEST_METHOD(FileLoader_Load_ExistingFile_Should_Return_Filestream)
+		TEST_METHOD(LoadImage_Valid_Path_Should_Load_Image_Correctly)
 		{
 			// Arrange
-
+			FrameData frameData;
+			EventDispatcher dispatcher;
+			VideoFacade videoEngine;
+			Drawable drawable;
+			SpriteObject spriteObject{ 1, 1, 1, 1, 1, "TestAssets/testPNG.png" };
+			drawable.registerSprite(1, &spriteObject);
 			// Act
-
+			videoEngine.loadImage(spriteObject);
 			// Assert
+			Assert::IsTrue(true);
+		}
 
+		TEST_METHOD(LoadImage_InValid_Should_ThrowError)
+		{
+			// Arrange
+			FrameData frameData;
+			EventDispatcher dispatcher;
+			VideoFacade videoEngine;
+			Drawable drawable;
+			SpriteObject spriteObject{ 1, 1, 1, 1, 1, NULL };
+			drawable.registerSprite(1, &spriteObject);
+			// Act
+			try {
+				videoEngine.loadImage(spriteObject);
+			}
+			// Assert
+			catch (exception e) {
+				Assert::AreEqual(e.what(), "Invalid file name");
+			}
+		}
+
+		TEST_METHOD(RenderCopy_Valid_File_Should_Render_Copy)
+		{
+			// Arrange
+			FrameData frameData;
+			EventDispatcher dispatcher;
+			VideoFacade videoEngine;
+			Drawable drawable;
+			SpriteObject spriteObject{ 1, 1, 1, 1, 1, "TestAssets/testPNG.png" };
+			drawable.registerSprite(1, &spriteObject);
+			drawable.changeToState(1);
+			// Act
+			videoEngine.renderCopy(drawable);
+			// Assert
+			Assert::IsTrue(true);
+		}
+
+		TEST_METHOD(RenderCopy_InValid_File_Should_ThrowError)
+		{
+			// Arrange
+			FrameData frameData;
+			EventDispatcher dispatcher;
+			VideoFacade videoEngine;
+			Drawable drawable;
+			SpriteObject spriteObject{ 1, 1, 1, 1, 1, NULL };
+			drawable.registerSprite(1, &spriteObject);
+			drawable.changeToState(1);
+			// Act
+			try {
+				videoEngine.renderCopy(drawable);
+			}
+			// Assert
+			catch (exception e) {
+				Assert::AreEqual(e.what(), "Invalid file name");
+			}
 		}
 	};
 }
