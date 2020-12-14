9d5501f55c3559afdf2260e86a203a96fba49f11
Wouter van Hees <whjhees@avans.nl>
2020-12-14 16:08:59 +0100

    moved doxygen to cpp

--- engineFoxtrot/Engine/Input/Commands/KeypressInvoker.cpp
+++ engineFoxtrot/Engine/Input/Commands/KeypressInvoker.cpp
@@ -2,13 +2,25 @@
 #include "stdafx.h"
 #include "KeypressInvoker.h"
 #include "Engine.h"
-
+/// @brief
+/// Register keybinds in the invoker for the first time, 
+/// throws exception when trying to register multiple commands under the same binding
+/// @param code
+/// The keycode for the command in the map
+/// @param command
+/// A shared pointer to the command which needs to be registered
 void KeypressInvoker::registerCommand(KeyCode code, ICommand* command) {
 	if (isRegistered(*command)) 
 		throw exception("keycode already registered, cannot override");
 	commands.insert(make_pair(code, command));
 };
 
+/// @brief
+/// Update the keycode that is set to this command
+/// @param code
+/// The new keycode for the command in the map
+/// @param command
+/// A shared pointer to the command which needs to be updated
 void KeypressInvoker::updateCommand(KeyCode code, ICommand* command) {
 
 	if (! isRegistered(*command))
@@ -31,6 +43,8 @@ void KeypressInvoker::updateCommand(KeyCode code, ICommand* command) {
 		commands[code] = command;
 }
 
+/// @brief
+/// Delete a command registered to the given keycode
 void KeypressInvoker::deleteCommandThatBelongsTo(const KeyCode& keycode) {
 	if (isRegistered(keycode))
 	{
@@ -38,6 +52,8 @@ void KeypressInvoker::deleteCommandThatBelongsTo(const KeyCode& keycode) {
 	}
 }
 
+/// @brief
+/// Handle commands placed in the command queue each frame
 void KeypressInvoker::executeCommandQueue(EventDispatcher& dispatcher)
 {
 	for (size_t i = 0; i < executionQueue.size(); i++)
@@ -49,6 +65,11 @@ void KeypressInvoker::executeCommandQueue(EventDispatcher& dispatcher)
 	}
 };
 
+/// @brief
+/// Attempts to execute a command for a given keycode,
+/// fails silently when no bind is registered to prevent unwanted exceptions
+/// @param code
+/// Keycode for the to execute command
 void KeypressInvoker::enqueueCommand(const KeyCode& code) {
 	if (isRegistered(code))
 		executionQueue.push(code);
