2ddaabda1960f9e0fea47ad6e153e357c7ce7be1
Lars Jansen <ljphjans@avans.nl>
2020-12-21 15:20:55 +0100

    More setups

--- Glitch/Game/Scenes/MappingScreen/MappingScene.cpp
+++ Glitch/Game/Scenes/MappingScreen/MappingScene.cpp
@@ -1,3 +1,38 @@
 #include "pch.h"
 #include "MappingScene.h"
 
+void MappingScene::onAttach()
+{
+}
+
+void MappingScene::onDetach()
+{
+}
+
+void MappingScene::start(bool playSound)
+{
+}
+
+void MappingScene::onUpdate(float deltaTime)
+{
+}
+
+void MappingScene::loadBackground()
+{
+}
+
+void MappingScene::loadMusic()
+{
+}
+
+void MappingScene::loadButtons()
+{
+}
+
+void MappingScene::loadSaveGame()
+{
+}
+
+void MappingScene::loadAchievements()
+{
+}
--- Glitch/Game/Scenes/MappingScreen/MappingScene.h
+++ Glitch/Game/Scenes/MappingScreen/MappingScene.h
@@ -7,6 +7,15 @@ public:
 	MappingScene(const int id, unique_ptr<Engine>& engine, shared_ptr<SceneStateMachine> _statemachine) : GameScene(id, engine, _statemachine) {};
 	~MappingScene() {};
 
-private:
+	void onAttach() override;
+	void onDetach() override;
+	void start(bool playSound) override;
+	virtual void onUpdate(float deltaTime) override;
 
+private:
+	void loadBackground();
+	void loadMusic();
+	void loadButtons();
+	void loadSaveGame();
+	void loadAchievements();
 };
