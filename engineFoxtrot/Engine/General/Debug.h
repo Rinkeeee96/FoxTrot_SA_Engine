185e8443a6432cfa0868871f4c159b057538eceb
WilliamRossAvans <44771477+WilliamRossAvans@users.noreply.github.com>
2020-12-28 15:14:13 +0100

    Update Debug.h

--- engineFoxtrot/Engine/General/Debug.h
+++ engineFoxtrot/Engine/General/Debug.h
@@ -3,7 +3,7 @@
 #ifndef DEBUG__H
 #define DEBUG__H
 
-#define DEBUG_VIEW true
+#define DEBUG_VIEW false
 #define DEBUG_MAIN false
 #define DEBUG_SCENE_MANAGER false
 #define DEBUG_PHYSICS_ENGINE false
