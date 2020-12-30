b13bbbe48495455e27ab1cf373b6750c6e6f2578
Rinkeeee96 <Rinkeeee96@users.noreply.github.com>
2020-12-30 11:55:46 +0000

    deploy: c616cc33e42d0f9cfefc3b587c5d3c6d8400f022

--- Glitch/class_tile_sprite.js
+++ Glitch/class_tile_sprite.js
@@ -3,5 +3,6 @@ var class_tile_sprite =
     [ "TileSprite", "class_tile_sprite.html#ad57756f0b0aaf178fd4d91a23adc6a63", null ],
     [ "height", "class_tile_sprite.html#af95d1e1c0d8187b59ac55cf867a6ba96", null ],
     [ "path", "class_tile_sprite.html#ab0cc3e1b84d921355d2308d6a018c3db", null ],
+    [ "properties", "class_tile_sprite.html#a675798269b4057a1a69b3c95360a9555", null ],
     [ "width", "class_tile_sprite.html#a6048c0449cd8fbaa1f08f8182cbfe568", null ]
 ];
