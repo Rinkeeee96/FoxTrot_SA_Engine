f55fe7a0b3e36b994798f1a4d3b499230daaff87
Max van Nistelrooij <maxvannistelrooij@gmail.com>
2020-12-30 12:52:38 +0100

    Fixed slime boss enemies not jumping

--- Glitch/Game/Characters/Enemies/SlimeBoss.cpp
+++ Glitch/Game/Characters/Enemies/SlimeBoss.cpp
@@ -106,7 +106,7 @@ void SlimeBoss::createSlime(int id, int startingTextureId, int x, int y) {
 		it++;
 	}
 	slime1->changeToState(0);
-	level.addNewObjectToLayer(4, slime1, true, false);
+	level.addNewObjectToLayer(5, slime1, true, false);
 }
 
 /// @brief
