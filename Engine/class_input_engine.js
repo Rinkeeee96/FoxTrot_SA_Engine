cc22e551cccb3d6daa0b20ad378858d0228ad97e
Rinkeeee96 <Rinkeeee96@users.noreply.github.com>
2020-12-30 11:54:50 +0000

    deploy: 4c1f39e3b6e7c1481269eaecad89943572c1e6d9

--- Engine/class_input_engine.js
+++ Engine/class_input_engine.js
@@ -2,15 +2,13 @@ var class_input_engine =
 [
     [ "InputEngine", "class_input_engine.html#a6a20294376216586c550c50ae736a817", null ],
     [ "~InputEngine", "class_input_engine.html#a87d0ba81ed29ed938fac7faa728a3663", null ],
-    [ "getSingleKeyStroke", "class_input_engine.html#a2ffd6a32a6671ce70f283fdfa0700e52", null ],
+    [ "onKeyPressed", "class_input_engine.html#ab7226dfe2df84a0c0279b411e9c90225", null ],
     [ "registerKeypressInvoker", "class_input_engine.html#aba8f5f8cbea770b91d8251def4e7db57", null ],
-    [ "shutdown", "class_input_engine.html#a1d95c85ea6f267f9f2d8635d6774eaa6", null ],
+    [ "shutdown", "class_input_engine.html#aa545a112b41abd4b1a9d0a82123eb894", null ],
     [ "start", "class_input_engine.html#a082f29fa0a4bd45802048ee560bbda06", null ],
     [ "update", "class_input_engine.html#a40f0b299416a98b334dee568fd4adf58", null ],
     [ "dispatcher", "class_input_engine.html#ab642ad1c2ba61666d8fb927254392d55", null ],
     [ "engine", "class_input_engine.html#a49def99fcbe6584862ac9dcf4de4c2b4", null ],
     [ "inputFacade", "class_input_engine.html#a88f8c9b9767327f6034c1209ad087349", null ],
-    [ "keypressInvoker", "class_input_engine.html#a04505e4f109e77e9ec99f54f8b259001", null ],
-    [ "onKeyPressed", "class_input_engine.html#a7c7c2a359d0ef3565dc18eeb2f39afe0", null ],
-    [ "onPause", "class_input_engine.html#aa27e9dc6eb86d5f0e44553214f2a041b", null ]
+    [ "keypressInvoker", "class_input_engine.html#a04505e4f109e77e9ec99f54f8b259001", null ]
 ];
--- Engine/struct_rectangle.js
+++
@@ -1,9 +0,0 @@
-var struct_rectangle =
-[
-    [ "Rectangle", "struct_rectangle.html#a500b93769fac80240d78ef3f92345c73", null ],
-    [ "overlaps", "struct_rectangle.html#a7e8e8458c985a6ce77ab701e2f89899d", null ],
-    [ "height", "struct_rectangle.html#a080c1530eb46073e9880b01d14c341fd", null ],
-    [ "width", "struct_rectangle.html#adbe8ae69919acc459e7ab155a69bc8ff", null ],
-    [ "x", "struct_rectangle.html#a40917b61b33bf046d189be401c13cc7f", null ],
-    [ "y", "struct_rectangle.html#a86f71f891bada4e5e920a4a9b7c505a3", null ]
-];
