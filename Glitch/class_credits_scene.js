988bb4cba609e1c663af8348dd982361d98c6768
Rinkeeee96 <Rinkeeee96@users.noreply.github.com>
2020-12-30 11:54:56 +0000

    deploy: 4c1f39e3b6e7c1481269eaecad89943572c1e6d9

--- Glitch/class_credits_scene.js
+++ Glitch/class_credits_scene.js
@@ -2,8 +2,6 @@ var class_credits_scene =
 [
     [ "CreditsScene", "class_credits_scene.html#a9be1f1943881ecff211ecbbef738f724", null ],
     [ "~CreditsScene", "class_credits_scene.html#aa6105bc2f1e6cb2898f399e96407f824", null ],
-    [ "handleAnimation", "class_credits_scene.html#aad98e16bff682b0e07d6892a898285c3", null ],
-    [ "loadAnimations", "class_credits_scene.html#a82e7ca964b6b3b413a1fb9741b386f2f", null ],
     [ "loadBackground", "class_credits_scene.html#a8946646eaab130c018ce51405748eb2c", null ],
     [ "loadButtons", "class_credits_scene.html#ae130e69c9167a09ebe7821cc88a61477", null ],
     [ "loadMusic", "class_credits_scene.html#a2969fdc236a806713a34861a0dddf6f1", null ],
@@ -12,8 +10,6 @@ var class_credits_scene =
     [ "onDetach", "class_credits_scene.html#a98dc4493294136470d233c99d73096b0", null ],
     [ "onUpdate", "class_credits_scene.html#aa1d22351d5b141a47aa51582606b8459", null ],
     [ "start", "class_credits_scene.html#a30b0aa0d030b3aa8b76e0704354a5bb5", null ],
-    [ "animation", "class_credits_scene.html#addc1bfd3bd6c6bd1f930abcf2398e58a", null ],
-    [ "falling", "class_credits_scene.html#ad95809ec3597ad309a91d6407967d482", null ],
     [ "onBackClick", "class_credits_scene.html#af3df15cce09d3300bbdc556c850e58e1", null ],
     [ "text", "class_credits_scene.html#a4754327fddfd423d48e0f0569f9e118f", null ]
 ];
